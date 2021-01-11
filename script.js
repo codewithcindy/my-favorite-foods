9109d6d405a0b1ce6358d67545d563571cfda07f
Cindy Taing <cindytaing94@gmail.com>
2021-01-11 12:21:48 -0800

    Add keydown listeners and dot navigation on bottom

--- script.js
+++ script.js
@@ -3,38 +3,79 @@
 const slides = document.querySelectorAll(".slide");
 const btnRight = document.querySelector(".slider__btn--right");
 const btnLeft = document.querySelector(".slider__btn--left");
+const dotContainer = document.querySelector(".dots");
 
-slides.forEach(function (slide, index) {
-  slide.style.transform = `translateX(${100 * index}%)`;
-});
-
-let curSlide = 0;
-const maxSlide = slides.length;
-
-btnRight.addEventListener("click", function () {
-  if (curSlide === maxSlide - 1) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
-
-  slides.forEach(function (slide, index) {
-    slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+const goToSlide = function (slide) {
+  slides.forEach(function (s, i) {
+    s.style.transform = `translateX(${100 * (i - slide)}%)`;
   });
-});
-
+};
+goToSlide(0);
 // transform: translateX %
 // page start up - curSlide = 0 / 0%, 100%, 200%, 300%, 400%, 500%
 // 1st click - curSlide = 1 / -100%, 0%, 100%, 200%, 300%, 400%
 
-btnLeft.addEventListener("click", function () {
+const prevSlide = function () {
   if (curSlide === 0) {
     curSlide = maxSlide - 1;
   } else {
     curSlide--;
   }
 
-  slides.forEach(function (slide, index) {
-    slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+  goToSlide(curSlide);
+  activateDot(curSlide);
+};
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  goToSlide(curSlide);
+  activateDot(curSlide);
+};
+
+const createDots = function () {
+  slides.forEach(function (s, i) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
   });
+};
+createDots();
+
+const activateDot = function (slide) {
+  // Remove all active dots
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  // Find current slide and add active dot
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+};
+activateDot(0);
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// Event Handlers
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") prevSlide();
+  if (e.key === "ArrowRight") nextSlide();
+});
+
+dotContainer.addEventListener("click", function (e) {
+  const slide = e.target.dataset.slide;
+
+  goToSlide(slide);
+  activateDot(slide);
 });
